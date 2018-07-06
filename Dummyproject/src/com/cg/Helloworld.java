cf6ada779f88c8d3b2e8c20bb07ac06718462916
bangade <bangade@DIN66006757.corp.capgemini.com>
2018-07-06 17:32:08 +0530

    This is Dummy code..

---
+++ Dummyproject/src/com/cg/Helloworld.java
@@ -0,0 +1,12 @@
+package com.cg;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+System.out.println("hi");
+System.out.println("thank you");
+	}
+
+}
